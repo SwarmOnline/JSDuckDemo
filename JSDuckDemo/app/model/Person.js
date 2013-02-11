fcfe3ce38a3e3d25bcc78549ff6962eaf3cd405a
Stuart Ashworth <stuart@stuartashworth.com>
2013-02-11 10:53:10 +0800

    Added optional docs to getAge method's parameter.

--- JSDuckDemo/app/model/Person.js
+++ JSDuckDemo/app/model/Person.js
@@ -1,7 +1,7 @@
-	/**
-	 * Holds data about a Person.
-	 */
-	Ext.define('JSDuckDemo.model.Person', {
+/**
+ * Holds data about a Person.
+ */
+Ext.define('JSDuckDemo.model.Person', {
 	extend: 'Ext.data.Model',
 
 	fields: [{
@@ -18,14 +18,13 @@
 
 	/**
 	 * Returns the Person's age in years.
-	 * @private
-	 * @param {Boolean} noRound If true this will return the value as a decimal with no rounding. If false the value with be rounded to the nearest year.
+	 * @param {Boolean} [noRounding=false] If true this will return the value as a decimal with no rounding. If false the value with be rounded to the nearest year.
 	 * @return {Number} The Person's age
 	 */
-	getAge: function(noRound){
+	getAge: function(noRounding){
 		var diffDays    = ((new Date()).getTime() - this.get('DateOfBirth').getTime()) /1000/(60 * 60 * 24),
 			age         = diffDays / 365.25;
 
-		return noRound ? age : Math.floor(age);
+		return noRounding ? age : Math.floor(age);
 	}
 });
